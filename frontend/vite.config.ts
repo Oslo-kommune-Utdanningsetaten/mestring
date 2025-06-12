57b2b59c7de3b2d5979c49e6b6bf75f4d6901c68
Thomas Drevon <thomas@drevon.no>
2025-06-12 09:42:51 +0200

    Fail early on missing env var

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,27 +2,27 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') }
-  console.log('Vite environment variables:', process.env)
 
-  const proxyConfig = process.env.VITE_API_BASE_URL
-    ? {
-        '/api': {
-          target: process.env.VITE_API_BASE_URL,
-          changeOrigin: true,
-          secure: false,
-          // rewrite: (path: string) => path.replace(/^\/api/, ''),
-        },
-        '/auth/': {
-          target: process.env.VITE_API_BASE_URL,
-          changeOrigin: true,
-          secure: false,
-          // rewrite: (path: string) => path.replace(/^\/api/, ''),
-        },
-      }
-    : undefined
+  if (!process.env.VITE_API_BASE_URL) {
+    throw new Error(
+      'VITE_API_BASE_URL is not set. All is lost! Unless you set it in your .env file, that is.'
+    )
+  }
+
+  const proxyConfig = {
+    '/api': {
+      target: process.env.VITE_API_BASE_URL,
+      changeOrigin: true,
+      secure: false,
+    },
+    '/auth/': {
+      target: process.env.VITE_API_BASE_URL,
+      changeOrigin: true,
+      secure: false,
+    },
+  }
 
   return {
     plugins: [svelte()],
