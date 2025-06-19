55c7a8d611c4ea09878e049138570778f573330c
Thomas Drevon <thomas@drevon.no>
2025-06-19 09:43:00 +0200

    Extract vertical input to separate component

--- frontend/src/components/ObservationEditVertical.svelte
+++ frontend/src/components/ObservationEditVertical.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
   import { dataStore } from '../stores/data'
   import { observationsCreate, observationsUpdate } from '../generated/sdk.gen'
-  import type { ObservationReadable, GoalReadable, UserReadable } from '../generated/types.gen'
+  import ValueInputVertical from './ValueInputVertical.svelte'
+  import type {
+    ObservationReadable,
+    GoalReadable,
+    UserReadable,
+    MasterySchemaReadable,
+  } from '../generated/types.gen'
+  import type { MasteryConfigLevel, MasterySchemaConfig } from '../types/models'
+
+  type MasterySchemaWithConfig = MasterySchemaReadable & {
+    config?: MasterySchemaConfig
+  }
 
   const { student, goal, observation, onDone } = $props<{
     student: UserReadable | null
@@ -10,22 +21,18 @@
     onDone: () => void
   }>()
 
-  const masterySchema = $derived(
+  const masterySchema: MasterySchemaWithConfig = $derived(
     $dataStore.masterySchemas.find(ms => ms.id === goal?.masterySchemaId)
   )
-  const origLevels = $derived(masterySchema?.config?.levels || [])
-  const masteryLevels = $derived(origLevels.reverse() || [])
-  const minValue = $derived(
-    masteryLevels.length ? masteryLevels[masteryLevels.length - 1].minValue : 1
-  )
-  const maxValue = $derived(masteryLevels.length ? masteryLevels[0].maxValue : 100)
-  const sliderValueIncrement = $derived(masteryLevels.length ? masteryLevels[0].increment || 1 : 1)
-  const widthMultiplier = $derived(masteryLevels.length ? 100 / masteryLevels.length : 1)
 
   let localObservation = $state<ObservationReadable>({
     ...observation,
     masteryValue: observation?.masteryValue || 50,
   })
+
+  function handleValueChange(newValue: number) {
+    localObservation.masteryValue = newValue
+  }
 
   async function handleSave() {
     localObservation.studentId = student?.id
@@ -58,43 +65,12 @@
   </h3>
 
   <div class="mb-4">
-    <label class="form-label" for="mastery-slider">
-      Hvor ofte mestrer {student?.name}: {goal?.title}
-    </label>
-
-    <div class="d-flex gap-3 position-relative">
-      <input
-        id="mastery-slider"
-        type="range"
-        min={minValue}
-        max={maxValue}
-        step={sliderValueIncrement}
-        class="slider"
-        bind:value={localObservation.masteryValue}
-      />
-
-      {#if masterySchema?.config?.isIncrementIndicatorEnabled}
-        <div
-          id="incrementIndicator"
-          style="top: calc(max(0px, {100 - localObservation.masteryValue}% - 3px));"
-        ></div>
-      {/if}
-
-      <div class="stairs-container">
-        {#each masteryLevels as masteryLevel, index}
-          <span
-            class="rung px-2"
-            style="width: {(index + 1) *
-              widthMultiplier}%; background-color: {(localObservation.masteryValue ?? 0) >=
-            masteryLevel.minValue
-              ? masteryLevel.color
-              : 'var(--bs-gray)'};"
-          >
-            {masteryLevel.title}
-          </span>
-        {/each}
-      </div>
-    </div>
+    <ValueInputVertical
+      {masterySchema}
+      masteryValue={localObservation.masteryValue ?? 50}
+      label="Hvor ofte mestrer {student?.name}: {goal?.title}"
+      onValueChange={handleValueChange}
+    />
   </div>
 
   <div class="d-flex gap-2 justify-content-start mt-4">
@@ -139,53 +115,4 @@
 </div>
 
 <style>
-  #incrementIndicator {
-    position: absolute;
-    top: 0;
-    left: 10%;
-    width: 90%;
-    height: 5px;
-    background-color: rgba(0, 0, 0, 0.25);
-    z-index: 1;
-  }
-
-  .stairs-container {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-end;
-    width: 100%;
-  }
-
-  .rung {
-    height: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: medium;
-  }
-
-  .slider {
-    margin: 0px 20px 0px 20px;
-    width: 10px;
-    writing-mode: vertical-lr;
-    direction: rtl;
-    padding-top: 0px;
-    background-color: var(--bs-gray);
-    z-index: 2;
-  }
-
-  .slider::-webkit-slider-thumb,
-  .slider::-moz-range-thumb {
-    width: 50px;
-    height: 10px;
-    border-radius: 3px;
-    cursor: pointer;
-  }
-
-  .slider::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    appearance: none;
-  }
 </style>
---
+++ frontend/src/components/ValueInputVertical.svelte
@@ -0,0 +1,123 @@
+<script lang="ts">
+  import type { MasteryConfigLevel, MasterySchemaConfig } from '../types/models'
+  import type { MasterySchemaReadable } from '../generated/types.gen'
+
+  type MasterySchemaWithConfig = MasterySchemaReadable & {
+    config?: MasterySchemaConfig
+  }
+
+  const {
+    masterySchema,
+    masteryValue = 50,
+    label = 'Mastery Value',
+    onValueChange,
+  } = $props<{
+    masterySchema: MasterySchemaWithConfig | null
+    masteryValue?: number
+    label?: string
+    onValueChange: (value: number) => void
+  }>()
+
+  const masteryLevels: MasteryConfigLevel[] = $derived(masterySchema?.config?.levels || [])
+  const sortedMasteryLevels: MasteryConfigLevel[] = $derived(
+    [...masteryLevels].sort((a, b) => b.minValue - a.minValue)
+  )
+  const minValue = $derived(
+    Math.min(...masteryLevels.map((lev: MasteryConfigLevel) => lev.minValue))
+  )
+  const maxValue = $derived(
+    Math.max(...masteryLevels.map((lev: MasteryConfigLevel) => lev.maxValue))
+  )
+  const sliderValueIncrement = $derived(masterySchema?.config?.inputIncrement || 1)
+  const widthMultiplier = $derived(masteryLevels.length ? 100 / masteryLevels.length : 1)
+  let value = $state(masteryValue)
+</script>
+
+<div class="mb-4">
+  <label class="form-label" for="mastery-slider">
+    {label}
+  </label>
+
+  <div class="d-flex gap-3 position-relative">
+    <input
+      id="mastery-slider"
+      type="range"
+      min={minValue}
+      max={maxValue}
+      step={sliderValueIncrement}
+      class="slider"
+      bind:value
+      oninput={() => onValueChange(value)}
+    />
+
+    {#if masterySchema?.config?.isIncrementIndicatorEnabled}
+      <div id="incrementIndicator" style="top: calc(max(0px, {100 - value}% - 3px));"></div>
+    {/if}
+
+    <div class="stairs-container">
+      {#each sortedMasteryLevels as masteryLevel, index}
+        <span
+          class="rung px-2"
+          style="width: {(index + 1) * widthMultiplier}%; background-color: {(value ?? 0) >=
+          masteryLevel.minValue
+            ? masteryLevel.color
+            : 'var(--bs-gray)'};"
+        >
+          {masteryLevel.title}
+        </span>
+      {/each}
+    </div>
+  </div>
+</div>
+
+<style>
+  #incrementIndicator {
+    position: absolute;
+    top: 0;
+    left: 10%;
+    width: 90%;
+    height: 5px;
+    background-color: rgba(0, 0, 0, 0.25);
+    z-index: 1;
+  }
+
+  .stairs-container {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-end;
+    width: 100%;
+  }
+
+  .rung {
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: medium;
+  }
+
+  .slider {
+    margin: 0px 20px 0px 20px;
+    width: 10px;
+    writing-mode: vertical-lr;
+    direction: rtl;
+    padding-top: 0px;
+    background-color: var(--bs-gray);
+    z-index: 2;
+  }
+
+  .slider::-webkit-slider-thumb,
+  .slider::-moz-range-thumb {
+    width: 50px;
+    height: 10px;
+    border-radius: 3px;
+    cursor: pointer;
+  }
+
+  .slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+  }
+</style>
