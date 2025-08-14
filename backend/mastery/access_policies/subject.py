a27406204910f6c0d5f64420d5ebc9b00b70f40c
Thomas Drevon <thomas@drevon.no>
2025-08-14 12:59:52 +0200

    Remove some logging

--- backend/mastery/access_policies/subject.py
+++ backend/mastery/access_policies/subject.py
@@ -23,20 +23,13 @@ class SubjectAccessPolicy(BasicAccessPolicy):
 
     def scope_queryset(self, request, qs):
         user = request.user
-        if getattr(user, 'is_superadmin', False):
-            logger.debug("SubjectAccessPolicy.scope_queryset superadmin user=%s", getattr(user, 'id', None))
+        if user.is_superadmin:
             return qs
         try:
             user_schools = user.get_schools()
-            logger.debug(
-                "SubjectAccessPolicy.scope_queryset user=%s schools=%s",
-                getattr(user, 'id', None),
-                list(user_schools.values_list('id', flat=True))
-            )
             return qs.filter(
                 Q(owned_by_school__in=user_schools) |
                 Q(group__school__in=user_schools)
             ).distinct()
         except Exception as e:
-            logger.debug("SubjectAccessPolicy.scope_queryset user=%s exception=%s", getattr(user, 'id', None), e)
             return qs.none()
