c0538a3ab8304e44b9214610481be9376cfe86bc
Thomas Drevon <thomas@drevon.no>
2025-09-25 14:24:51 +0200

    Also consider user_school affiliations as roles

--- backend/mastery/access_policies/base.py
+++ backend/mastery/access_policies/base.py
@@ -1,11 +1,14 @@
 from rest_access_policy import AccessPolicy
 from django.contrib.auth.models import AnonymousUser
 
+
 class BaseAccessPolicy(AccessPolicy):
     group_prefix = "role:"
 
+    # Return list of strings representing the user's roles
+    # Does not include info about where these roles are held (e.g. which school or group)
     def get_user_group_values(self, user) -> list[str]:
-        # shorcircuit AnonymouseUser
+        # shorcircuit AnonymousUser
         if isinstance(user, AnonymousUser):
             return []
 
@@ -13,8 +16,10 @@ class BaseAccessPolicy(AccessPolicy):
         if user.is_superadmin:
             values.append("superadmin")
         try:
-            # Collect role names from memberships, e.g. 'teacher', 'student'
+            # Collect user <--> group affiliations passing through the user_group table (e.g. teacher, student)
             values.extend({ug.role.name for ug in user.user_groups.all()})
+            # Collect user <--> school affiliations passing through the user_school table (e.g. school admin)
+            values.extend({us.role.name for us in user.user_schools.all()})
         except Exception:
             pass
         return values
