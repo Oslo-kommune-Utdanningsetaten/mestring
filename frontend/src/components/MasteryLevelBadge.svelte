ec37848948d1bf7ab0f4f792916806bda04264da
Thomas Drevon <thomas@drevon.no>
2025-10-20 14:03:56 +0200

    Less sharp non-goal badge

--- frontend/src/components/MasteryLevelBadge.svelte
+++ frontend/src/components/MasteryLevelBadge.svelte
@@ -96,7 +96,7 @@
     background: repeating-linear-gradient(
       -45deg,
       color-mix(in srgb, var(--bs-danger) 30%, transparent),
-      color-mix(in srgb, var(--bs-danger) 30%, transparent) 2px,
+      color-mix(in srgb, var(--bs-danger) 30%, transparent) 1px,
       white 2px,
       white 4px
     );
