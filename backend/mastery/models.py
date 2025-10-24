45434602f8cb3591df295e124b4ec26009accd0b
Thomas Drevon <thomas@drevon.no>
2025-10-24 10:48:49 +0200

    school config for toggle goal.title

--- backend/mastery/models.py
+++ backend/mastery/models.py
@@ -40,6 +40,7 @@ class School(BaseModel):
     is_service_enabled = models.BooleanField(default=False)
     is_group_goal_enabled = models.BooleanField(default=True)  # can group goals can be created
     is_student_list_enabled = models.BooleanField(default=False)  # can teachers see the /students menu item
+    is_goal_title_enabled = models.BooleanField(default=True)  # are goals displayed with titles
     # which subjects can be used: 'only-custom' (owned by school), 'only-group', 'all'
     subjects_allowed = models.CharField(max_length=50, null=False, default='all')
 
