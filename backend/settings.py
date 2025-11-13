7be5b3ad722213de93c33bdba271afc809590caf
Thomas Drevon <thomas@drevon.no>
2025-11-13 09:19:37 +0100

    Update security settings

--- backend/settings.py
+++ backend/settings.py
@@ -46,10 +46,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'mastery.middleware.CamelCaseQueryParamMiddleware',
@@ -96,10 +96,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://iz-mest-ap01t.oslo.int',
     'http://iz-mest-ap01p.oslo.int'
-
 ]
 
-CSRF_COOKIE_HTTPONLY = False  # Requires different handling of the token to work with HttpOnly cookies
+CSRF_COOKIE_HTTPONLY = True
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'Strict'
 
@@ -107,7 +106,7 @@ SESSION_COOKIE_AGE = 28800  # 8 hours, matching Feide expires_in
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_SAMESITE = 'Lax'
+SESSION_COOKIE_SAMESITE = 'Strict'
 
 # cdn.jsdelivr.net is needed for swagger-ui
 CSP_DEFAULT_SRC = ("'self'", "'unsafe-inline'", "cdn.jsdelivr.net")
@@ -255,6 +254,3 @@ LOGGING = {
         },
     },
 }
-
-SESSION_COOKIE_AGE = 28800
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
