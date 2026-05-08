6afef3d30bdb22a1d9d930167033b903a284a549
Thomas Drevon <thomas@drevon.no>
2026-05-08 09:40:00 +0200

    Tweakage

--- frontend/src/components/StatusTag.svelte
+++ frontend/src/components/StatusTag.svelte
@@ -15,22 +15,22 @@
       | undefined
   )
 
-  const color = $derived(
-    masterySchema && status.masteryValue != null
-      ? getMasteryLevelColorByValue(status.masteryValue, masterySchema)
-      : 'rgba(100, 100, 100)'
-  )
-
   const title = $derived(
     masterySchema && status.masteryValue != null
       ? getMasteryTitleByValue(status.masteryValue, masterySchema)
       : ''
   )
 
-  const textColor = $derived(getContrastFriendlyTextColor(color))
+  const boxColor = $derived(
+    masterySchema && status.masteryValue != null
+      ? getMasteryLevelColorByValue(status.masteryValue, masterySchema)
+      : 'rgba(100, 100, 100)'
+  )
+
+  const textColor = $derived(getContrastFriendlyTextColor(boxColor))
 </script>
 
-<span title={status.id} class="status-tag" style="background-color: {color}; color: {textColor}">
+<span title={status.id} class="status-tag" style="background-color: {boxColor}; color: {textColor}">
   {title}
 </span>
 
@@ -39,6 +39,8 @@
     display: inline-flex;
     align-items: center;
     justify-content: center;
+    padding-top: 1px;
+    border-radius: 2px;
     width: 30px;
     height: 30px;
     font-size: 0.8rem;
